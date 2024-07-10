d0a4804ab5c94098c222948d192615e3cc953386
Thiago Imai <Tigulima@gmail.com>
2024-07-10 17:01:37 -0300

    alg II trab 2 template added

---
+++ s2/alg2/2024-i/trabalho/t2/ordenacao.c
@@ -0,0 +1,41 @@
+#include "ordenacao.h"
+
+#include <string.h>
+
+void getNome(char nome[]) {
+    strncpy(nome, "Thiago Imai Lima", MAX_CHAR);
+    // adicionada terminação manual para caso de overflow
+    nome[MAX_CHAR - 1] = '\0';
+}
+
+uint32_t getGRR() { return 20204397; }
+
+uint64_t mergeSort(int vetor[], size_t tam) {
+    vetor[0] = 99;
+    return -1;
+}
+
+uint64_t quickSort(int vetor[], size_t tam) {
+    vetor[0] = 99;
+    return -1;
+}
+
+uint64_t heapSort(int vetor[], size_t tam) {
+    vetor[0] = 99;
+    return -1;
+}
+
+uint64_t mergeSortSR(int vetor[], size_t tam) {
+    vetor[0] = 99;
+    return -1;
+}
+
+uint64_t quickSortSR(int vetor[], size_t tam) {
+    vetor[0] = 99;
+    return -1;
+}
+
+uint64_t heapSortSR(int vetor[], size_t tam) {
+    vetor[0] = 99;
+    return -1;
+}
