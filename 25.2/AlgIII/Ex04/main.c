963b0c1c0c5516f4e07350be95d423a0a8e64410
Tigulima <tigulima@gmail.com>
2025-08-22 16:18:31 -0300

    Ex04 de alg3

---
+++ 25.2/AlgIII/Ex04/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct nodo {
+    char chave;
+    struct nodo *pai;
+    struct nodo *fd;
+    struct nodo *fe;
+};
+
+void transplantar(struct nodo *arvore, struct nodo *sai, struct nodo *entra) {
+    return;
+}
+
+void excluir(struct nodo *arvore, struct nodo *n) {
+    if (n->fe == NULL) {
+        transplantar(arvore, n, n->fd);
+        return;
+    }
+    if (n->fd == NULL) {
+        transplantar(arvore, n, n->fe);
+        return;
+    }
+}
+
+int main() {
+
+}
